b8207e9215e36aab4ad40ff0e765ed25e5db01a2
ankurbansal <ankurbasnal05@gmail.com>
2025-04-21 15:48:23 -0500

    removed open api key

--- src/main/java/dev/nano/sbot/constant/Constants.java
+++ src/main/java/dev/nano/sbot/constant/Constants.java
@@ -18,23 +18,23 @@ public class Constants {
 
 
     public static final String PROMPT_TEMPLATE_2 = """ 
-                    You are an expert in Spring Boot documentation. You have comprehensive knowledge of all Spring Boot concepts, features, configuration, and best practices.
-                    Your answers will be awesome because they thoroughly explain Spring Boot concepts in a way that is easy for anyone to understand.
+                    You are an expert on american express credit cards. You have comprehensive knowledge of all american express credit cards.
+                    Your answers will be awesome because they thoroughly explain features of american express credit cards in a way that is easy for anyone to understand.
                     Your answers will be clear because they are concise, use simple language, and avoid unclear jargon.
                     Your answers will be detailed because they provide helpful examples, sample code, and references to additional resources when needed.
                     Your answers will be good because they are accurate, address the user's specific question, and follow best practices.
-                    If you do not know the answer to a question, respond by directing the user to official Spring Boot documentation that contains the relevant information while acknowledging you do not have the specific answer.
-                    When responding, remember to be awesome, clear, detailed and good in explaining Spring Boot concepts to users of all skill levels.
+                    If you do not know the answer to a question, respond by directing the user to official american express credit card documentation that contains the relevant information while acknowledging you do not have the specific answer.
+                    When responding, remember to be awesome, clear, detailed and good in explaining american express credit cards features to users of all skill levels.
                     
-                    Using your comprehensive knowledge of Spring Boot documentation, please answer the following question :
+                    Using your comprehensive knowledge of american express Credit Card documentation, please answer the following question :
                     {{question}}
                     
-                    Base your answer exclusively and solely on the following information from the Spring Boot documentation:
+                    Base your answer exclusively and solely on the following information from the american express Credit Card documentation:
                     {{information}}
                 """;
 
     public static final List<String> SPRING_BOOT_RESOURCES_LIST = List.of(
-            "https://docs.spring.io/spring-boot/docs/current/reference/html/index.html"
+            "https://www.americanexpress.com/us/credit-cards/?category=all&intlink=US-Axp-Shop-Consumer-VAC-CardMember-all-StickyFilter"
             //"https://www.tutorialspoint.com/spring_boot/index.htm",
             //"https://howtodoinjava.com/spring-boot2/",
             //"https://reflectoring.io/categories/spring-boot/",
